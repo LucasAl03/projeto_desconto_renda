e8de78f0ca5380c499dcb4090e6912ce7bf66ef5
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-06-19 18:31:51 -0300

    criado o arquivo scripts.js e linkado ao index.html

---
+++ js/scripts_pessoa.js
@@ -0,0 +1,9 @@
+const inputNome = document.querySelector('#nome')
+const inputIdade = document.querySelector('#idade')
+const inputRenda = document.querySelector('#renda')
+const divLista = document.querySelector('#div-lista-pessoas')
+const btnAdc = document.querySelector('#submit')
+
+btnAdc.addEventListener('click', (evt)=>{
+    
+})
